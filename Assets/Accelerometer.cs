72b54f0ef02ee8f3f9f6d213a1ccf758e70f5009
ytrivedi1 <trivediy1@udayton.edu>
2020-12-17 19:12:39 -0500

    Initial commit

---
+++ Assets/Accelerometer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Accelerometer : MonoBehaviour
+{
+    public bool isFlat = true;
+    private Rigidbody rigid;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigid = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector3 tilt = Input.acceleration;
+        if(isFlat)
+        {
+           tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+        rigid.AddForce(tilt);
+        Debug.DrawRay(transform.position + Vector3.up, tilt, Color.cyan);
+    }
+}
